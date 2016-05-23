d63ead484a71628e039277783879f4729fe4568a
jan <jan@infinity.(none)>
2016-05-23 20:41:00 +0300

    PseudoPrimes

---
+++ others/src/others/PseudoPrimes.java
@@ -0,0 +1,45 @@
+
+package others;
+
+
+import java.util.HashMap;
+
+public class PseudoPrimes {
+
+    public boolean isPseudoPrime(Integer pnumber) {
+        Integer tmp = (pnumber - 1) / 2;
+        HashMap<Integer, Integer> perm2 = new HashMap();
+
+        for (int i = 1; i <= tmp; i++) {
+            perm2.put(i, 2 * i);
+            perm2.put(i + tmp, 2 * i - 1);
+        }
+
+        Integer pn = 1;
+        Integer counter = 0;
+
+        do {
+            counter++;
+            System.out.println(pn);
+            pn = perm2.get(pn);
+        } while (!pn.equals(1));
+
+        if ((pnumber - 1) % counter == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        PseudoPrimes ip = new PseudoPrimes();
+//341,561
+        System.out.println(ip.isPseudoPrime(341));
+
+//        for (int i = 3; i < 1000; i +=2) {
+//            if (ip.isPrime(i)) {
+//                System.out.println(i);
+//            }
+//        }
+    }
+}
